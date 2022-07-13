c47e00543221cdc5223a8ea1f5961b9748f2fb4d
rbxbx <rbxbxdev@gmail.com>
2022-07-12 22:39:16 -0700

    Basic gameplay loop action

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,92 @@
 use bracket_lib::prelude::*;
 
+#[derive(Debug)]
 enum GameMode {
     Menu,
     Playing,
     End,
 }
 
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 75.0;
+
+#[derive(Debug)]
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y, velocity: 0.0 }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0,
+                self.y,
+                YELLOW,
+                BLACK,
+                to_cp437('@')
+        );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.y += self.velocity as i32;
+        self.x += 1;
+
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
+#[derive(Debug)]
 struct State {
+    frame_time: f32,
     mode: GameMode,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
         Self {
+            frame_time: 0.0,
             mode: GameMode::Menu,
+            player: Player::new(5, 25),
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
+
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
@@ -54,6 +122,8 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.frame_time = 0.0;
+        self.player = Player::new(5, 25);
         self.mode = GameMode::Playing;
     }
 }
